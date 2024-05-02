fdbb876efd008909a6adb5c0b48bfbc7502acf9c
AleksandrsShenkarenko <144392025+AleksandrsShenkarenko@users.noreply.github.com>
2024-05-02 18:58:13 +0000

    02.05.24

--- heap.py
+++ heap.py
@@ -62,7 +62,7 @@ def find_kth_smallest(nums, k):
         max_heap.insert(num)
     for num in nums[k:]:
         if num < max_heap.heap[0]:
-            max_heap.remove()
+            max_heap.remov()
             max_heap.insert(num)
     return max_heap.heap[0]
 
@@ -82,3 +82,33 @@ for i in range(len(nums)):
     print(f'Test passed: {result == expected_outputs[i]}')
     print('---------------------------------------')
 
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Test case 1...
+    Input: [3, 2, 1, 5, 6, 4] with k = 2
+    Output: 2
+    Expected output: 2
+    Test passed: True
+    ---------------------------------------
+    Test case 2...
+    Input: [6, 5, 4, 3, 2, 1] with k = 3
+    Output: 3
+    Expected output: 3
+    Test passed: True
+    ---------------------------------------
+    Test case 3...
+    Input: [1, 2, 3, 4, 5, 6] with k = 4
+    Output: 4
+    Expected output: 4
+    Test passed: True
+    ---------------------------------------
+    Test case 4...
+    Input: [3, 2, 3, 1, 2, 4, 5, 5, 6] with k = 7
+    Output: 5
+    Expected output: 5
+    Test passed: True
+    ---------------------------------------
+
+"""
